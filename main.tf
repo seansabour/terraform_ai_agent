62dc40589541afa83e6d1af4f7db906207bffa15
Sean Sabour <seansabour@Seans-MacBook-Pro.local>
2025-04-11 23:04:26 -0600

    Add generated Terraform code

--- main.tf
+++ main.tf
@@ -1,25 +1,29 @@
-resource "aws_s3_bucket" "public_bucket" {
-  bucket = "my-public-bucket"
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_public_bucket" {
+  bucket = "my-public-bucket-unique-name"
 
   acl    = "public-read"
 
   tags = {
-    Name        = "MyPublicBucket"
+    Name        = "My public S3 bucket"
     Environment = "Production"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.public_bucket.id
+  bucket = aws_s3_bucket.my_public_bucket.id
 
   block_public_acls       = false
-  block_public_policy     = false
   ignore_public_acls      = false
+  block_public_policy     = false
   restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.public_bucket.id
+  bucket = aws_s3_bucket.my_public_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -28,12 +32,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         Effect = "Allow"
         Principal = "*"
         Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.public_bucket.arn}/*"
+        Resource = "${aws_s3_bucket.my_public_bucket.arn}/*"
       }
     ]
   })
-}
-
-provider "aws" {
-  region = "us-west-2"
 }
