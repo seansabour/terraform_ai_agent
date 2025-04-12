86e031bbde89a166d804cb0d84f42708d0d58ceb
Sean Sabour <seansabour@Seans-MacBook-Pro.local>
2025-04-11 23:19:41 -0600

    Add generated Terraform code

--- main.tf
+++ main.tf
@@ -2,38 +2,38 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "my_public_bucket" {
+resource "aws_s3_bucket" "public_bucket" {
   bucket = "my-public-bucket-unique-name"
 
   acl    = "public-read"
 
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
-    Name        = "My public S3 bucket"
+    Name        = "MyPublicBucket"
     Environment = "Production"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.my_public_bucket.id
+  bucket = aws_s3_bucket.public_bucket.id
 
-  block_public_acls       = false
-  ignore_public_acls      = false
-  block_public_policy     = false
+  block_public_acls   = false
+  ignore_public_acls  = false
+  block_public_policy = false
   restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.my_public_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.my_public_bucket.arn}/*"
-      }
-    ]
-  })
 }
