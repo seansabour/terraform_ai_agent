b6e879b81a66f56e98e145c93f17db36c9f47657
Sean Sabour <seansabour@Seans-MacBook-Pro.local>
2025-04-11 23:30:55 -0600

    Add generated Terraform code

--- main.tf
+++ main.tf
@@ -3,31 +3,34 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "private_bucket" {
-  bucket = "my-private-bucket-123456"
+  bucket = "my-private-bucket"
 
   acl    = "private"
 
-  tags = {
-    Name        = "My Private Bucket"
-    Environment = "Production"
+  versioning {
+    enabled = true
   }
-}
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.private_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  bucket = aws_s3_bucket.private_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
+  }
+
+  lifecycle_rule {
+    id      = "log"
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
+
+  tags = {
+    Name        = "PrivateBucket"
+    Environment = "Production"
   }
 }
 
@@ -38,11 +41,4 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_logging" "logging" {
-  bucket = aws_s3_bucket.private_bucket.id
-
-  target_bucket = "my-logs-bucket"
-  target_prefix = "logs/"
 }
